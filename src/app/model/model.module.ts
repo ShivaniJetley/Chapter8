69bf90308caded9254040d30b8fcc0c4bd24d632
ShivaniJetley <email.shivanijetley@gmail.com>
2020-05-07 00:20:52 -0700

    Add order model and respository

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core'
 import { ProductRepository } from './product.repository'
 import { StaticDataSource } from './static.datasource'
 import { Cart } from './cart.model'
+import { Order } from './order.model'
+import { OrderRepository } from './order.repository'
 
 // make its classes/services available to other modules
 @NgModule({
-  providers: [ProductRepository, StaticDataSource, Cart],
+  providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository],
 })
 export class ModelModule {}
